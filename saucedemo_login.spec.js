5954f9f4b923dc356c11c4bf3517fd81a56bcf1d
Alex <vitaemagister@gmail.com>
2023-11-28 21:59:48 +0200

    Added gitignore for credentials and using credentials in test

--- saucedemo_login.spec.js
+++ saucedemo_login.spec.js
@@ -1,16 +1,19 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+import {user} from './credentials.js';
 
 test('has title', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
+  await page.pause();
   await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').fill(user.login);
   await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="password"]').fill(user.password);
+  await page.pause();
   await page.locator('[data-test="login-button"]').click();
   await page.getByRole('button', { name: 'Open Menu' }).click();
   await page.getByRole('link', { name: 'Logout' }).click();
-  await page.pause();
+
   
 
 });
